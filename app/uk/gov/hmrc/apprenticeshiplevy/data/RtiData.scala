77763a28f99a63eb0259878246b0cd983e897f1f
Emanuele Blanco <emanuele.blanco@gmail.com>
2016-07-07 16:24:41 +0100

    Added a new EPS endpoint.

---
+++ app/uk/gov/hmrc/apprenticeshiplevy/data/RtiData.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apprenticeshiplevy.data
+
+import org.joda.time.LocalDate
+
+object RtiData {
+
+  val data: Map[String, EmployerPaymentSummary] = Map(
+    "123/AB12345" -> EmployerPaymentSummary(
+      EmpRefs(officeNumber = "123", payeRef = "Ref/001<Q>", aoRef = "123PQ7654321X"),
+      Some("yes"),
+      Some(DateRange(from = new LocalDate(2016, 4, 6), to = new LocalDate(2017, 4, 5))),
+      Some(DateRange(from = new LocalDate(2015, 4, 6), to = new LocalDate(2015, 5, 5))),
+      Some("yes"),
+      Some(RecoverableAmountsYTD(
+        taxMonth = Some(7),
+        smpRecovered = Some(BigDecimal("13.12")),
+        sppRecovered = Some(BigDecimal("13.12")),
+        sapRecovered = Some(BigDecimal("13.12")),
+        shppRecovered = Some(BigDecimal("13.12")),
+        nicCompensationOnSMP = Some(BigDecimal("13.12")),
+        nicCompensationOnSPP = Some(BigDecimal("13.12")),
+        nicCompensationOnSAP = Some(BigDecimal("13.12")),
+        nicCompensationOnShPP = Some(BigDecimal("13.12")),
+        cisDeductionsSuffered = Some(BigDecimal("13.12"))
+      )),
+      Some(Account(
+        accountHoldersName = "Mr. James T Bacon",
+        accountNo = "12345678",
+        sortCode = "123456",
+        buildingSocRef = Some("SMITH-(123)")
+      )),
+      "16-17",
+      Some(FinalSubmission(
+        becauseSchemeCeased = Some("yes"),
+        dateSchemeCeased = Some(new LocalDate(2017, 1, 1)),
+        forYear = Some("yes")
+      )))
+  )
+}
--- app/uk/gov/hmrc/apprenticeshiplevy/data/domain.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/data/domain.scala
@@ -54,3 +54,61 @@ case class Fractions(empref: String, fractionCalculations: List[FractionCalculat
 object Fractions {
   implicit val formats = Json.format[Fractions]
 }
+
+// EPS data
+
+case class EmpRefs(officeNumber: String, payeRef: String, aoRef: String)
+
+object EmpRefs {
+  implicit val formats = Json.format[EmpRefs]
+}
+
+case class DateRange(from: LocalDate, to: LocalDate)
+
+object DateRange {
+  implicit val formats = Json.format[DateRange]
+}
+
+case class RecoverableAmountsYTD(taxMonth: Option[Int] = None,
+                                 smpRecovered: Option[BigDecimal] = None,
+                                 sppRecovered: Option[BigDecimal] = None,
+                                 sapRecovered: Option[BigDecimal] = None,
+                                 shppRecovered: Option[BigDecimal] = None,
+                                 nicCompensationOnSMP: Option[BigDecimal] = None,
+                                 nicCompensationOnSPP: Option[BigDecimal] = None,
+                                 nicCompensationOnSAP: Option[BigDecimal] = None,
+                                 nicCompensationOnShPP: Option[BigDecimal] = None,
+                                 cisDeductionsSuffered: Option[BigDecimal] = None)
+
+object RecoverableAmountsYTD {
+  implicit val formats = Json.format[RecoverableAmountsYTD]
+}
+
+case class Account(accountHoldersName: String,
+                   accountNo: String,
+                   sortCode: String,
+                   buildingSocRef: Option[String] = None)
+
+object Account {
+  implicit val formats = Json.format[Account]
+}
+
+case class FinalSubmission(becauseSchemeCeased: Yes, dateSchemeCeased: Option[LocalDate], forYear: Yes)
+
+object FinalSubmission {
+  implicit val formats = Json.format[FinalSubmission]
+}
+
+case class EmployerPaymentSummary(empRefs: EmpRefs,
+                                  noPaymentForPeriod: Yes,
+                                  noPaymentDates: Option[DateRange],
+                                  periodOfInactivity: Option[DateRange],
+                                  empAllceInd: YesNo,
+                                  recoverableAmountsYTD: Option[RecoverableAmountsYTD],
+                                  account: Option[Account],
+                                  relatedTaxYear: String,
+                                  finalSubmission: Option[FinalSubmission])
+
+object EmployerPaymentSummary {
+  implicit val formats = Json.format[EmployerPaymentSummary]
+}
