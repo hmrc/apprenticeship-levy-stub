0d621138c243b8643d4ff4501fb1e30f2457053d
Doug Clinton <doug@wellfactored.com>
2016-08-12 17:22:24 +0100

    Fixed up the model after pulling LevyDeclarations into its own file

---
+++ app/uk/gov/hmrc/apprenticeshiplevy/data/LevyDeclarations.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apprenticeshiplevy.data
+
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.{LocalDate, LocalDateTime}
+import play.api.libs.json._
+
+case class PayrollPeriod(year: String, month: Int)
+
+object PayrollPeriod {
+  implicit val formats = Json.format[PayrollPeriod]
+}
+
+
+case class LevyDeclaration(id: Long,
+                           submissionTime: LocalDateTime,
+                           dateCeased: Option[LocalDate] = None,
+                           inactiveFrom: Option[LocalDate] = None,
+                           inactiveTo: Option[LocalDate] = None,
+                           payrollPeriod: Option[PayrollPeriod] = None,
+                           levyDueYTD: Option[BigDecimal] = None,
+                           levyAllowanceForFullYear: Option[BigDecimal] = None,
+                           noPaymentForPeriod: Option[Boolean] = None)
+
+
+object LevyDeclaration {
+  implicit val ldtFormats = new Format[LocalDateTime] {
+    val fmt = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+
+    override def reads(json: JsValue): JsResult[LocalDateTime] = implicitly[Reads[JsString]].reads(json).map { js =>
+      fmt.parseDateTime(js.value).toLocalDateTime
+    }
+
+    override def writes(o: LocalDateTime): JsValue = JsString(fmt.print(o))
+  }
+
+  implicit val formats = Json.format[LevyDeclaration]
+}
+
+case class LevyDeclarations(empref: String, declarations: Seq[LevyDeclaration])
+
+object LevyDeclarations {
+  implicit val formats = Json.format[LevyDeclarations]
+}
--- app/uk/gov/hmrc/apprenticeshiplevy/data/domain.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/data/domain.scala
@@ -20,12 +20,6 @@ import org.joda.time.format.DateTimeFormat
 import org.joda.time.{LocalDate, LocalDateTime}
 import play.api.libs.json._
 
-case class PayrollPeriod(year: String, month: Int)
-
-object PayrollPeriod {
-  implicit val formats = Json.format[PayrollPeriod]
-}
-
 case class Fraction(region: String, value: BigDecimal)
 
 object Fraction {
@@ -36,38 +30,6 @@ case class FractionCalculation(calculatedAt: LocalDate, fractions: List[Fraction
 
 object FractionCalculation {
   implicit val formats = Json.format[FractionCalculation]
-}
-
-case class LevyDeclaration(id: Long,
-                           submissionTime: LocalDateTime,
-                           dateCeased: Option[LocalDate] = None,
-                           inactiveFrom: Option[LocalDate] = None,
-                           inactiveTo: Option[LocalDate] = None,
-                           payrollPeriod: Option[PayrollPeriod] = None,
-                           levyDueYTD: Option[BigDecimal] = None,
-                           levyAllowanceForFullYear: Option[BigDecimal] = None,
-                           noPaymentForPeriod: Option[Boolean] = None)
-
-
-object LevyDeclaration {
-  implicit val ldtFormats = new Format[LocalDateTime] {
-    val fmt = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
-
-    override def reads(json: JsValue): JsResult[LocalDateTime] = implicitly[Reads[JsString]].reads(json).map { js =>
-      fmt.parseDateTime(js.value).toLocalDateTime
-    }
-
-    override def writes(o: LocalDateTime): JsValue = JsString(fmt.print(o))
-  }
-
-  implicit val formats = Json.format[LevyDeclaration]
-}
-
-
-case class LevyDeclarations(empref: String, schemeCessationDate: Option[LocalDate], declarations: Seq[LevyDeclaration])
-
-object LevyDeclarations {
-  implicit val formats = Json.format[LevyDeclarations]
 }
 
 case class Fractions(empref: String, fractionCalculations: List[FractionCalculation])
