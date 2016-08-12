70e7ae5677c23f954e5d9f8bacb25f3856fbc417
Doug Clinton <doug@wellfactored.com>
2016-08-12 17:36:16 +0100

    Can now load declaration test data and convert it back to an equivalent EPS. Lets us use the DAS team's test data for the stub

--- app/uk/gov/hmrc/apprenticeshiplevy/data/RtiData.scala
+++ app/uk/gov/hmrc/apprenticeshiplevy/data/RtiData.scala
@@ -22,30 +22,53 @@ object RtiData {
 
   def eps(empref: String): Option[Seq[EmployerPaymentSummary]] = {
     val e = empref.replace("/", "")
+    loadEPSData(empref).orElse(buildFromDeclarationData(empref))
+  }
+
+  def loadEPSData(empref: String): Option[Seq[EmployerPaymentSummary]] = {
+    val e = empref.replace("/", "")
     val path = s"/sandbox_data/employers/$e/eps.json"
+
     Option(this.getClass.getResourceAsStream(path)).flatMap { is =>
       val json = scala.io.Source.fromInputStream(is).getLines().mkString("\n")
       Json.parse(json).asOpt[Seq[EmployerPaymentSummary]]
-    }.orElse {
-      val path = s"/sandbox_data/employers/$e/declarations.json"
-      Option(this.getClass.getResourceAsStream(path)).flatMap { is =>
-        val json = scala.io.Source.fromInputStream(is).getLines().mkString("\n")
-        Json.parse(json).\("declarations").asOpt[Seq[LevyDeclaration]].map { decls =>
-          decls.map(convertToEPS(EmpRefs.fromEmpref(empref), _))
-        }
+    }
+  }
+
+  def buildFromDeclarationData(empref: String): Option[Seq[EmployerPaymentSummary]] = {
+    val e = empref.replace("/", "")
+    val path = s"/sandbox_data/employers/$e/declarations.json"
+
+    Option(this.getClass.getResourceAsStream(path)).flatMap { is =>
+      val json = scala.io.Source.fromInputStream(is).getLines().mkString("\n")
+      Json.parse(json).\("declarations").asOpt[Seq[LevyDeclaration]].map { decls =>
+        decls.map(convertToEPS(EmpRefs.fromEmpref(empref), _))
       }
     }
   }
 
   def convertToEPS(emprefs: EmpRefs, decl: LevyDeclaration): EmployerPaymentSummary = {
     decl match {
-      case LevyDeclaration(id, ts, Some(ceasedDate), _, _, _, _, _, _) =>
+      case LevyDeclaration(id, ts, Some(ceasedDate), _, _, Some(payrollPeriod), _, _, _) =>
         EmployerPaymentSummary(id, ts, emprefs, None, None, None, None, None, Some(levy(decl)),
           None, decl.payrollPeriod.get.year,
-          Some(FinalSubmission(Some("Yes"), decl.dateCeased, decl.payrollPeriod.map(_.year))))
+          Some(FinalSubmission(Some("Yes"), decl.dateCeased, Some(payrollPeriod.year))))
+
+      case LevyDeclaration(id, ts, None, Some(inactiveFrom), Some(inactiveTo), Some(payrollPeriod), _, _, _) =>
+        EmployerPaymentSummary(id, ts, emprefs, None, None,
+          Some(DateRange(inactiveFrom, inactiveTo)),
+          None, None, None, None, payrollPeriod.year, None)
+
+      case LevyDeclaration(id, ts, _, _, _, Some(payrollPeriod), _, _, Some(_)) =>
+        EmployerPaymentSummary(id, ts, emprefs, Some("yes"), None, None,
+          None, None, None, None, payrollPeriod.year, None)
+
+      case LevyDeclaration(id, ts, None, _, _, Some(payrollPeriod), Some(levy), Some(allowance), _) =>
+        EmployerPaymentSummary(id, ts, emprefs, None, None, None, None, None,
+          Some(ApprenticeshipLevy(levy, payrollPeriod.month, allowance)), None, payrollPeriod.year, None)
     }
   }
 
   def levy(decl: LevyDeclaration) = ApprenticeshipLevy(decl.levyDueYTD.get, decl.payrollPeriod.get.month, decl.levyAllowanceForFullYear.get)
-  
+
 }
